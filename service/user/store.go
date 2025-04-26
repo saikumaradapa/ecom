338167ee2331b77beba8df7622eb6aeb8752f4c4
saikumaradapa <saikumaradapa3266@gmail.com>
2025-04-27 01:49:10 +0530

    add user models

---
+++ service/user/store.go
@@ -0,0 +1,55 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/saikumaradapa/ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewMySQLStorage(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
