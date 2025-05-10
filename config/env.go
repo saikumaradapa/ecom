6eba9c5c492c7a02573081ebeb7d957f8db729bc
saikumaradapa <saikumaradapa3266@gmail.com>
2025-05-10 16:50:24 +0530

    add loging and jwt auth

--- config/env.go
+++ config/env.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -14,6 +15,8 @@ type Config struct {
 	Passwd     string
 	DBAddress  string
 	DBName     string
+	JWTExpirationInSeconds int64
+	JWTSecret string
 }
 
 var Envs = initConfig()
@@ -28,12 +31,27 @@ func initConfig() Config {
 		Passwd:     getEnv("DB_PASSWORD", "mypassword"),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
 		DBName:     getEnv("DB_NAME", "ecom"),
+		JWTSecret: getEnv("JWT_SECRET", "not-secret-secret-anymore?"),
+		JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600 * 24 * 7),
 	}
 }
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
+	}
+
+	return fallback
+}
+
+func getEnvAsInt(key string, fallback int64) int64 {
+	if value, ok := os.LookupEnv(key); ok {
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return fallback
+		}
+
+		return i
 	}
 
 	return fallback
--- service/user/store.go
+++ service/user/store.go
@@ -36,6 +36,27 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return u, nil
 }
 
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
+}
+
 func (s *Store) CreateUser(user types.User) error {
 	query := `
 		INSERT INTO users (firstName, lastName, email, password)
